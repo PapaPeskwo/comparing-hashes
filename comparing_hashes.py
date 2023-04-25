b4516a81d80ec7e69bf2c15814c0b7ba5a06d96b
PapaPeskwo <58036451+PapaPeskwo@users.noreply.github.com>
2023-04-25 16:18:46 +0200

    no need to paste the path anymore, file explorer is used now

--- comparing_hashes.py
+++ comparing_hashes.py
@@ -1,21 +1,18 @@
 import subprocess, platform
+import tkinter as tk
+from tkinter import filedialog
 
 def generate_sha256(filename):
     if platform.system() == "Windows":
-        # Generate SHA-256 hash value of file using certutil command
         sha256 = subprocess.check_output(["certutil", "-hashfile", filename, "SHA256"]).split()[-6]
     else:
-        # Generate SHA-256 hash value of file using certutil command
         sha256 = subprocess.check_output(["sha256sum", filename]).split()[0]
-    # Remove the newline character from the end of the hash value
+
     sha256 = sha256.decode("utf-8").rstrip()
-    # Return the SHA-256 hash value as a string
     return sha256
 
 def compare_hashes(filename, known_sha256):
-    # Generate SHA-256 hash value of file using certutil command
     file_sha256 = generate_sha256(filename)
-    # Print the known and file hash values
     print(f"\nKnown SHA-256 hash value: {known_sha256}")
     print(f"File SHA-256 hash value: {file_sha256}")
     if file_sha256 == known_sha256:
@@ -29,9 +26,14 @@ def compare_hashes(filename, known_sha256):
         print(f"THE FILE: \n{filename} \nMAY HAVE BEEN TAMPERED WITH.")
         print('!!!' + '=' * 94 + '!!!')
 
-# Take input from user for the file name and known SHA-256 hash value
-filename = input("Enter the path to the filename from root folder:\n")
-known_sha256 = input("Enter the known SHA-256 hash value:\n").lower()
+def open_file_dialog():
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename()
+    return file_path
 
-# Call the compare_hashes function with the user inputs
+print("\n---Select your file when the file explorer opens---")
+known_sha256 = input("\nEnter the known SHA-256 hash value:\n").lower().strip()
+filename = open_file_dialog()
+
 compare_hashes(filename, known_sha256)
