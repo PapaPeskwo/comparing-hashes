a94bd15a96c6b3d6dc3e2b1d7976c874c1791e4c
PapaPeskwo <58036451+PapaPeskwo@users.noreply.github.com>
2023-06-04 15:17:12 +0200

    added sha1 support

--- comparing_hashes.py
+++ comparing_hashes.py
@@ -1,6 +1,16 @@
-import subprocess, platform
+import subprocess
+import platform
 import tkinter as tk
 from tkinter import filedialog
+
+def generate_sha1(filename):
+    if platform.system() == "Windows":
+        sha1 = subprocess.check_output(["certutil", "-hashfile", filename, "SHA1"]).split()[-6]
+    else:
+        sha1 = subprocess.check_output(["sha1sum", filename]).split()[0]
+
+    sha1 = sha1.decode("utf-8").rstrip()
+    return sha1
 
 def generate_sha256(filename):
     if platform.system() == "Windows":
@@ -11,11 +21,11 @@ def generate_sha256(filename):
     sha256 = sha256.decode("utf-8").rstrip()
     return sha256
 
-def compare_hashes(filename, known_sha256):
-    file_sha256 = generate_sha256(filename)
-    print(f"\nKnown SHA-256 hash value: {known_sha256}")
-    print(f"File SHA-256 hash value: {file_sha256}")
-    if file_sha256 == known_sha256:
+def compare_hashes(filename, known_hash, hash_function):
+    file_hash = hash_function(filename)
+    print(f"\nKnown hash value: {known_hash}")
+    print(f"File hash value: {file_hash}")
+    if file_hash == known_hash:
         print()
         print('+' + '=' * 98 + '+')
         print(f"The file: \n{filename} \nis authentic.")
@@ -33,7 +43,13 @@ def open_file_dialog():
     return file_path
 
 print("\n---Select your file when the file explorer opens---")
-known_sha256 = input("\nEnter the known SHA-256 hash value:\n").lower().strip()
+hash_type = int(input("\nEnter the type of hash (1)SHA1 or (2)SHA256):\n"))
+known_hash = input("\nEnter the known hash value:\n").lower().strip()
 filename = open_file_dialog()
 
-compare_hashes(filename, known_sha256)
+if hash_type == 1:
+    compare_hashes(filename, known_hash, generate_sha1)
+elif hash_type == 2:
+    compare_hashes(filename, known_hash, generate_sha256)
+else:
+    print("Invalid hash type. Please select SHA1 or SHA256.")
