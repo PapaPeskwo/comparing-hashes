a2cb81376e8ce6622fbd880f526841d5570cecd3
PapaPeskwo <tomi.orlovac@gmail.com>
2023-06-07 17:44:17 +0300

    added md5 support

--- comparing_hashes.py
+++ comparing_hashes.py
@@ -5,6 +5,7 @@ import time
 import tkinter as tk
 from tkinter import filedialog
 
+
 def generate_sha1(filename):
     if platform.system() == "Windows":
         sha1 = subprocess.check_output(["certutil", "-hashfile", filename, "SHA1"]).split()[-6]
@@ -28,6 +29,14 @@ def generate_sha512(filename):
         sha512 = subprocess.check_output(["sha512sum", filename]).split()[0]
     sha512 = sha512.decode("utf-8").rstrip()
     return sha512
+
+def generate_md5(filename):
+    if platform.system() == "Windows":
+        md5 = subprocess.check_output(["certutil", "-hashfile", filename, "MD5"]).split()[-6]
+    else:
+        md5 = subprocess.check_output(["md5sum", filename]).split()[0]
+    md5 = md5.decode("utf-8").rstrip()
+    return md5
 
 def compare_hashes(filename, known_hash, hash_function):
     file_hash = hash_function(filename)
@@ -61,27 +70,34 @@ def hash_sha512():
     known_hash = input("\nEnter the known SHA-512 hash value:\n").lower().strip()
     open_file_dialog(known_hash, generate_sha512)
 
+def hash_md5():
+    known_hash = input("\nEnter the known MD5 hash value:\n").lower().strip()
+    open_file_dialog(known_hash, generate_md5)
+
 # Menu
 while True:
     os.system('cls' if os.name == 'nt' else 'clear')
     print("\nHash Comparison")
     print("Select a hash function")
-    print("1. SHA1")
-    print("2. SHA256")
-    print("3. SHA512")
-    print("4. Exit")
+    print("1. MD5")
+    print("2. SHA1")
+    print("3. SHA256")
+    print("4. SHA512")
+    print("5. Exit")
 
-    choice = input("\nEnter your choice (1-4): ")
+    choice = input("\nEnter your choice (1-5): ")
 
     if choice == '1':
-        hash_sha1()
+        hash_md5()
     elif choice == '2':
-        hash_sha256()
+        hash_sha1()
     elif choice == '3':
-        hash_sha512()
+        hash_sha256()
     elif choice == '4':
+        hash_sha512()
+    elif choice == '5':
         print("Exiting.")
         break
     else:
-        print("Invalid choice. Please enter a number between 1 and 4.")
+        print("Invalid choice. Please enter a number between 1 and 5.")
         time.sleep(2)
